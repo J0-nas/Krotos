90dd63b06215b4d3a90cce03b625fca13c0b94b9
Jonas Halstrup <jonashalstrup@Jonass-MacBook-Air.local>
2017-04-08 12:48:24 +0200

    implemented json mapping from str to tuple for genres and genreTopTracks

--- lib/krotos.ex
+++ lib/krotos.ex
@@ -1,5 +1,5 @@
 defmodule Krotos do
-  use HTTPoison.Base
+  use HTTPoison.Base, Poison
   @moduledoc """
   Client for using the Napster Api.
   Relies on the system enviroment variable NAPSTER_API_KEY to issue the requests.
@@ -15,21 +15,58 @@ defmodule Krotos do
 
   def listGenres do
     url = @baseURL <> "genres"
-    headers = [apikey: key()]
+    #headers = [apikey: key()]
     case HTTPoison.get url, [apikey: key()] do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        #IO.puts ("Success: " <> body)
-        handleResponse(:listGenres, body)
+        Krotos.ResponseHandler.handleResponse(:listGenres, body)
       {:error, %HTTPoison.Error{reason: reason}} -> IO.inspect reason
       _ -> IO.puts "Unknown Error"
     end
   end
 
-  def handleResponse(:listGenres, body) do
-    :listGenres
+  def getTopTracksForGenre(genre) do
+    getTopTracksForGenre(genre, 0)
   end
 
-  def handleResponse(:getTopTracksForGenre) do
+  def getTopTracksForGenre(genre, limit) do
+    url = @baseURL <> "genres/" <> genre <> "/tracks/top"
+    url = if limit > 0 and limit <= 200 do
+       url <> "?limit=#{limit}"
+    end
 
+    case HTTPoison.get url, [apikey: key()] do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        Krotos.ResponseHandler.handleResponse(:getTopTracksForGenre, body)
+      {:error, %HTTPoison.Error{reason: reason}} -> IO.inspect reason
+      _ -> IO.puts "Unknown Error"
+    end
+  end
+
+  defmodule ResponseHandler do
+    def decode(json) do
+      Poison.decode json
+    end
+
+    def handleResponse(:listGenres, body) do
+      case decode body do
+        {:ok, dict} ->
+          buildGenreList dict
+        #_ -> IO.puts "Error decoding the json"
+      end
+    end
+
+    def handleResponse(:getTopTracksForGenre, body) do
+      case decode body do
+        {:ok, dict} -> buildTracksList dict
+      end
+    end
+
+    def buildGenreList(dict) do
+      Enum.map(dict["genres"], fn(x) -> {x["id"], x["name"], x["links"]["childGenres"]["ids"]} end)
+    end
+
+    def buildTracksList(dict) do
+      Enum.map(dict["tracks"], fn(x) -> {x["id"], x["name"], x["artistName"], x["albumName"], x["previewURL"]} end)
+    end
   end
 end
